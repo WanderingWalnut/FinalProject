20bbf561ef64bc97507728ad3e4799322e31257f
Programwiz1 <monil.patel2@ucalgary.ca>
2024-08-03 18:16:28 -0600

    UpdatedNewsFetcher
    The previous version had errors for the display portion.

--- SubwayScreen/src/main/java/ca/ucalgary/edu/ensf380/NewsFetcher.java
+++ SubwayScreen/src/main/java/ca/ucalgary/edu/ensf380/NewsFetcher.java
@@ -1,51 +1,54 @@
 package ca.ucalgary.edu.ensf380;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JTextArea;
 import java.util.Timer;
 import java.util.TimerTask;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
+import javax.swing.JTextArea;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NewsFetcher {
     private static final String API_KEY = "6IoGr7aC0nWvQaD8yIMnwtWKgl0jXocbvvUR8AAZ";
-    private static final String API_URL = "https://api.thenewsapi.com/v1/news/top?api_token=" + API_KEY;
+    private static final String API_URL = "https://api.thenewsapi.com/v1/news/all?api_token=" + API_KEY;
 
-    public static List<String> fetchNews(String keyword) throws Exception {
+    public static List<String> fetchNews(String keyword) throws IOException {
+        OkHttpClient client = new OkHttpClient();
         String urlString = API_URL + "&search=" + keyword;
         System.out.println("Fetching news from URL: " + urlString); // Debugging line
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
 
-        int responseCode = conn.getResponseCode();
+        Request request = new Request.Builder()
+                .url(urlString)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        int responseCode = response.code();
         System.out.println("Response Code: " + responseCode); // Debugging line
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
-        }
-        reader.close();
+        if (responseCode == 200) {
+            String responseBody = response.body().string();
+            System.out.println("Response: " + responseBody); // Debugging line
 
-        System.out.println("Response: " + response.toString()); // Debugging line
-
-        // Parse JSON response
-        List<String> newsTitles = new ArrayList<>();
-        JSONObject jsonResponse = new JSONObject(response.toString());
-        JSONArray articles = jsonResponse.getJSONArray("data");
-        for (int i = 0; i < articles.length(); i++) {
-            JSONObject article = articles.getJSONObject(i);
-            String title = article.getString("title");
-            newsTitles.add(title);
+            // Parse JSON response
+            List<String> newsTitles = new ArrayList<>();
+            JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+            JsonArray articles = jsonObject.getAsJsonArray("data");
+            for (JsonElement article : articles) {
+                String title = article.getAsJsonObject().get("title").getAsString();
+                newsTitles.add(title);
+            }
+            return newsTitles;
+        } else {
+            throw new IOException("Failed to fetch news: " + responseCode);
         }
-        return newsTitles;
     }
 
     public static void startNewsScrolling(JTextArea textArea) {
